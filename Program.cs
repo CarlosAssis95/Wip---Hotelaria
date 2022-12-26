92dad5a826c309ec708b86f18f48e45ab93f7161
CarlosAssis <calos.dudu1502@gmail.com>
2022-12-26 19:15:38 -0300

    Implementado valor nas suites

--- Program.cs
+++ Program.cs
@@ -9,14 +9,8 @@ class Program
     enum menu {cadastrarHospedes = 1, listarHospedes, darBaixaEmHospede, sair}
     // LISTA QUE FAZ O RECEBE AS PESSOAS NO PROGRAMA HOTEL.
     static List<Pessoa> pList = new List<Pessoa>();
-
     static void Main(string [] args)
     {
-
-        // OPÇÕES DE SUITE DISPONIVEIS NO PROGRAMA HOTEL 
-        Suite Basica = new Suite("Basica" , 2, 30m);
-        Suite Mediana = new Suite("Mediana", 3, 50m);
-        Suite Premium = new Suite("Premium", 4, 100m);
         bool exibirMenu = true;
 
         Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-");
@@ -79,8 +73,8 @@ class Program
         }
     // CHAMADA DA FUNÇÃO ESCOLHER SUITE DENTRO DO CADASTRO DE HOSPEDES
     escolherSuite();
-    
     }     
+    
     // FUNCAO DE ESCOLHA DE SUITES
     static void escolherSuite()
     {
@@ -89,47 +83,47 @@ class Program
         int opcaoSuite = int.Parse(Console.ReadLine());
         int Basica = 1;
         int Mediana = 2;
-        int Premium = 3; 
+        int valorSuite; 
 
         if (opcaoSuite == Basica)
         {
+            valorSuite = 30;
             Console.WriteLine($"Você escolheu a suite Basica");
         }
         else if(opcaoSuite == Mediana)
         {
+            valorSuite = 50;
             Console.WriteLine($"Você escolheu a suite Mediana");
         }
         else 
         {
+            valorSuite = 100;
             Console.WriteLine($"Você escolheu a suite Premium");
         }
-
-        // VARIAVEIS COM VALOR DA DIARIA DE CADA SUITE
-        Basica = 40;
-        Mediana = 50;
-        Premium = 100;
-
+       
         // CAPTURA A QUANTIDADE DE DIAS QUE VAI ESTAR HOSPEDADO
         Console.WriteLine("Vai estar conosco por quantos dias?");
         int dias = int.Parse(Console.ReadLine()); 
-        decimal total = opcaoSuite * dias;  // FAZ O CALCULO DO VALOR DA DIARIA X OS DIAS QUE VAI ESTAR HOPEDADO
+        decimal total =  valorSuite * dias;  // FAZ O CALCULO DO VALOR DA DIARIA X OS DIAS QUE VAI ESTAR HOPEDADO
 
-        Console.WriteLine($"Voce vai pagar o total de {total} Reais");
+        Console.WriteLine($"Total ficou em {total} Reais");
         Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-
     }
+    
     // FUNÇÃO QUE LISTA OS HOSPEDES CADASTRADOS NA MINHA LISTA
     static void listarHospedes()
     {
         Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=");
         Console.WriteLine("Os Hospedes atuais são");
+        
         foreach(Pessoa p in pList)
         {
             Console.WriteLine($"{p.Nome}, {p.CPF}, {p.Nascimento}");
         }
         Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=");
     }
-    // FUNÇÃO QUE RETIRA OS HOSPEDES DA MINHA LISTA DANDO BAIXA NO HOTEL
+    
+    // FUNÇÃO QUE RETIRA OS HOSPEDES DA LISTA DANDO BAIXA NO HOTEL
     static void darBaixaEmHospede()
     {
         Console.WriteLine("Dando baixa em hospede");
